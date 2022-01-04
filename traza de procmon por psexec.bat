db7ba2b10b3638d8c463b6cd2f107c48e6cede5b
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-04 13:03:50 -0300

    Prueba

--- traza de procmon por psexec.bat
+++ traza de procmon por psexec.bat
@@ -4,11 +4,14 @@ PsExec64.exe -sd \\hostdestino c:\procmon64.exe -accepteula -backingfile c:\temp
 REM luego de "\\hostdestino" sigue la ruta del ejecutable "procmon64.exe" en el destino, ubicado en su disco raiz.
 REM Se indican los parametros y seguido, se guarda un fichero ".pml" , todo de manera silenciosa "-quiet"
 msg * Ejecutando msiexec... (misma sintaxis del lanswp)
+REM --------------------------------------------------------------------------------------------------------------
 msiexec.exe /i "\\lansweeper1\DefaultPackageShare$\Installers\Firefox.msi" /norestart /qn
+REM Arriba, es la linea que se ejecuta desde el Lansw
 PAUSE
-Psexec64.exe -sd \\intende_03_726 c:\procmon64.exe -accepteula -terminate -quiet
+REM --------------------------------------------------------------------------------------------------------------
+Psexec64.exe -sd \\hostdestino c:\procmon64.exe -accepteula -terminate -quiet
 msg * "cerrando la traza del processmonitor"
-xcopy \\intende_03_726\c$\temp\proc.pml c:\
+xcopy \\hostdestino\c$\temp\proc.pml c:\
 c:\procmon64.exe /openlog c:\proc.pml
 PAUSE
 REM Ubicacion del ejecutable a copiar (procmon64) hacia host destino:
