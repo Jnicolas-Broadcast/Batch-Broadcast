2328b362e3b30d808bf9363f087d21fa03e35b2c
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-04 14:05:10 -0300

    Prueba

--- traza de procmon por psexec.bat
+++ traza de procmon por psexec.bat
@@ -5,8 +5,11 @@ REM luego de "\\X.X.X.X" sigue la ruta del ejecutable "procmon64.exe" ubicado en
 REM Se indican los parametros y seguido, se guarda un fichero ".pml" , todo de manera silenciosa "-quiet"
 Psexec64.exe -sd \\10.1.5.3 c:\procmon64.exe -accepteula -terminate -quiet
 msg * "concluye la traza del processmonitor"
-xcopy \\10.1.5.3\c$\proc.pml c:\
+xcopy \\10.1.5.3\c$\proc.pml \\SISTEMA_14_712\c$
 c:\procmon64.exe /openlog c:\proc.pml
 PAUSE
+
+
+
 REM Ubicacion del ejecutable a copiar (procmon64) hacia host destino:
 REM \\X.X.X.X\c$\Program Files (x86)\Lansweeper\PackageShare\Installers
