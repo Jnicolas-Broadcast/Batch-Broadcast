0145827171167d84dbe2cd3c416d7d1ca021ef93
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-04 14:02:06 -0300

    Prueba

--- traza de procmon por psexec.bat
+++ traza de procmon por psexec.bat
@@ -1,12 +1,11 @@
-REM @ECHO OFF
+@ECHO OFF
 cd c:\
-PsExec64.exe -sd \\SISTEMA_12_705 c:\procmon64.exe -accepteula -backingfile c:\proc.pml -quiet
-REM luego de "\\SISTEMA_12_705" sigue la ruta del ejecutable "procmon64.exe" ubicado en ese SISTEMA_12_705, y en su disco raiz.
+PsExec64.exe -sd \\10.1.5.3 c:\procmon64.exe -accepteula -backingfile c:\proc.pml -quiet
+REM luego de "\\X.X.X.X" sigue la ruta del ejecutable "procmon64.exe" ubicado en ese X.X.X.X, y en su disco raiz.
 REM Se indican los parametros y seguido, se guarda un fichero ".pml" , todo de manera silenciosa "-quiet"
-msg * Ejecutando msiexec... (misma sintaxis del lanswp)
-Psexec64.exe -sd \\SISTEMA_12_705 c:\procmon64.exe -accepteula -terminate -quiet
+Psexec64.exe -sd \\10.1.5.3 c:\procmon64.exe -accepteula -terminate -quiet
 msg * "concluye la traza del processmonitor"
-xcopy \\SISTEMA_12_705\c$\temp\proc.pml c:\
+xcopy \\10.1.5.3\c$\proc.pml c:\
 c:\procmon64.exe /openlog c:\proc.pml
 PAUSE
 REM Ubicacion del ejecutable a copiar (procmon64) hacia host destino:
