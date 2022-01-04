b87124b5a543cad51784772a964de04202075c89
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-04 13:36:32 -0300

    Prueba

--- traza de procmon por psexec.bat
+++ traza de procmon por psexec.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 cd c:\
-PsExec64.exe -sd \\SISTEMA_14_712 c:\procmon64.exe -accepteula -backingfile c:\temp\proc.pml -quiet
-REM luego de "\\SISTEMA_14_712" sigue la ruta del ejecutable "procmon64.exe" ubicado en ese SISTEMA_14_712, y en su disco raiz.
+PsExec64.exe -sd \\SISTEMA_12_705 c:\procmon64.exe -accepteula -backingfile c:\temp\proc.pml -quiet
+REM luego de "\\SISTEMA_12_705" sigue la ruta del ejecutable "procmon64.exe" ubicado en ese SISTEMA_12_705, y en su disco raiz.
 REM Se indican los parametros y seguido, se guarda un fichero ".pml" , todo de manera silenciosa "-quiet"
 msg * Ejecutando msiexec... (misma sintaxis del lanswp)
 REM --------------------------------------------------------------------------------------------------------------
@@ -9,9 +9,9 @@ msiexec.exe /i "\\lansweeper1\DefaultPackageShare$\Installers\Firefox.msi" /nore
 REM Arriba, es la linea que se ejecuta desde el Lansw
 PAUSE
 REM --------------------------------------------------------------------------------------------------------------
-Psexec64.exe -sd \\SISTEMA_14_712 c:\procmon64.exe -accepteula -terminate -quiet
+Psexec64.exe -sd \\SISTEMA_12_705 c:\procmon64.exe -accepteula -terminate -quiet
 msg * "concluye la traza del processmonitor"
-xcopy \\SISTEMA_14_712\c$\temp\proc.pml c:\
+xcopy \\SISTEMA_12_705\c$\temp\proc.pml c:\
 c:\procmon64.exe /openlog c:\proc.pml
 PAUSE
 REM Ubicacion del ejecutable a copiar (procmon64) hacia host destino:
