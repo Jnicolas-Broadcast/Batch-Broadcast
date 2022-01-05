4536cd4fa28b9aca6184b2b713c79e66c3a3e62e
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-05 11:38:45 -0300

    Prueba

--- Task-Acrobat.bat
+++ Task-Acrobat.bat
@@ -1,4 +1,8 @@
 rem @ECHO OFF
+rem xcopy \\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Acro reader\
+rem cd C:\%USERPROFILE%\Desktop\Acro reader\
+rem Aqui abajo ejecutamos un wmic call uninstall y quitamos el viejo acrobat
+rem -------------------------------------------------------------------------------------------------------------------
 
-cd C:\%USERPROFILE%\Desktop\Acro reader\
-msiexec.exe /i "{PackageShare}\Installers\Adobe_Reader.msi" /norestart /qn
+rem -------------------------------------------------------------------------------------------------------------------
+msiexec.exe /i "{PackageShare}\Installers\Acro reader\AcroRead.msi" /norestart /qn
