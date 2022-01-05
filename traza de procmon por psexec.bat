195fd5456240ba396e6c240e9723d21c66bb0b30
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-05 17:36:15 -0300

    Prueba

--- traza de procmon por psexec.bat
+++ traza de procmon por psexec.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+title Traza de procmon por psexec
 cd c:\
 PsExec64.exe -sd \\CONTADU_03_717 c:\procmon64.exe -accepteula -backingfile c:\proc.pml -quiet
 REM luego de "\\X.X.X.X" sigue la ruta del ejecutable "procmon64.exe" ubicado en ese X.X.X.X, y en su disco raiz.
