d41aa69e6016ecc1817e43c8ec0e0fd38617f327
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-05 14:34:50 -0300

    Prueba

--- Wmic test.bat
+++ Wmic test.bat
@@ -1,3 +1,5 @@
 title Ejecucion wmic
+@echo OFF
 rem Ejemplar , revisar el uso de comillas simples cuando el paquete lleva espacios
+wmic
 product where "name ='Adobe Acrobat Reader DC - Español'" call uninstall /nointeractive
