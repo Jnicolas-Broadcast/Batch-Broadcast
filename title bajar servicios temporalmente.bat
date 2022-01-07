f068345011673c69ac665294eff0008a614bb1f9
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-07 09:58:48 -0300

    Prueba

--- title bajar servicios temporalmente.bat
+++ title bajar servicios temporalmente.bat
@@ -1,10 +1,22 @@
 title bajar servicios temporalmente
 :: La idea es restablecer servicios para correr los deploys sin problemas.
 @ECHO OFF
+:: ts a "Windows installer esta ejecutandose continuamente en w10"
+:: https://answers.microsoft.com/en-us/windows/forum/all/windows-installer-is-always-running-on-windows-10/9ac0f602-4797-4c61-b976-9a46081a887c
+:: Se extraen indicaciones del topic y se utilizan (con algunos cambios) a continuación
 net stop wuauserv
 net stop cryptSvc
 net stop bits
 net stop msiserver
+::ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+::ren C:\Windows\System32\catroot2 Catroot2.old
+timeout 7 /nobreak
+net start wuauserv
+net start cryptSvc
+net start bits
 taskkill /im OpenOffice417.exe /f /t
+taskkill /im OfficeClickToRun.exe /f /t
+taskkill /im msiexec.exe /f /t
+net start msiserver
+ 
 
-::taskkill /im msiexec.exe /f /t
