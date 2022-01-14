9ef82059729a42d56633ce688c359d89d931bdbe
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-14 12:09:22 -0300

    Test

---
+++ Reinstalar Adobe DC 21.007.20099.bat
@@ -0,0 +1,5 @@
+title Reinstalar Adobe Reader DC 21.007.20099
+:: la linea debajo es para ejecutar (como test) en localhost, luego dejar comentado.
+msiexec.exe /x "AcroRead.msi" /norestart /qn
+:: para lanzar en Lansweeper:
+:: msiexec.exe /x "{PackageShare}\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
