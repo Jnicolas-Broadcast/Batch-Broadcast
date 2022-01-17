6119716a46905b9c059954bfc27a68662b5d33f0
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-17 17:10:13 -0300

    test

--- Disable-services.txt
+++
@@ -1,6 +0,0 @@
-title Deshabilitar servicios especificos
-@echo off
-net stop wuauserv
-net stop cryptSvc
-net stop bits
-net stop msiserver
--- Reinstalar Adobe DC 21.007.20099.bat
+++ Reinstalar Adobe DC 21.007.20099.bat
@@ -1,19 +1,24 @@
 title Reinstalar Adobe Reader DC 21.007.20099
 :: ----------------------------------------------------------------------------------------------------------------------------------------
-:: DESDE LANSWEEPER:
-:: xcopy "C:\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" "Destino equipo final\disco\ubicacion" /f /z 
+:: https://www.lansweeper.com/Forum/yaf_postsm44107_Copying-a-file-from-deploymentshare-to-Local-Machine-and-Create-Shortcut-on-Local-Machine.aspx#
+REM Command: cmd.exe /c xcopy.exe /Y {PackageShare}\Installers\PATH_TO\YOUR_FILES\*.* "C:\PATH TO\DESTINATION"
 :: ----------------------------------------------------------------------------------------------------------------------------------------
+:: runas /user:administrador /password:password "cmd.exe /c xcopy.exe /Y '\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\*.*' 'C:\' /f"
+REM cmd.exe /c xcopy.exe /Y "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\*.*" "C:\"
 
+PAUSE
 ::eliminamos..
 msiexec.exe /x "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
+:: espera de 3 minutos
+timeout 180 /nobreak
 ::instalamos..
 msiexec.exe /i "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
 timeout 300 /nobreak
 :: espera de 5 minutos
 :: actualizamos..
 msiexec.exe /p "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRdrDCUpd2100720099.msp" /norestart /qn
-:: espera de 7 minutos
-timeout 420 /nobreak
+:: espera de 4 minutos
+timeout 240 /nobreak
 taskkill /im msiexec.exe /f /t
 net stop msiserver
 timeout 7 /nobreak
