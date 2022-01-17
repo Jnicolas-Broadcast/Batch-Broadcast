595f9e4ecf1c709a590e0d2df4554d4f9c39b9d8
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-17 12:55:11 -0300

    test

--- Reinstalar Adobe DC 21.007.20099.bat
+++ Reinstalar Adobe DC 21.007.20099.bat
@@ -1,5 +1,22 @@
 title Reinstalar Adobe Reader DC 21.007.20099
-:: la linea debajo es para ejecutar (como test) en localhost, luego dejar comentado.
-msiexec.exe /x "AcroRead.msi" /norestart /qn
-:: para lanzar en Lansweeper:
-:: msiexec.exe /x "{PackageShare}\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
+:: ----------------------------------------------------------------------------------------------------------------------------------------
+:: DESDE LANSWEEPER:
+:: xcopy "C:\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" /f /z 
+:: ----------------------------------------------------------------------------------------------------------------------------------------
+
+::eliminamos..
+msiexec.exe /x "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
+::instalamos..
+msiexec.exe /i "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRead.msi" /norestart /qn
+timeout 300 /nobreak
+:: espera de 5 minutos
+:: actualizamos..
+msiexec.exe /p "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Nuevo Acrobat Reader\AcroRdrDCUpd2100720099.msp" /norestart /qn
+:: espera de 7 minutos
+timeout 420 /nobreak
+taskkill /im msiexec.exe /f /t
+net stop msiserver
+timeout 7 /nobreak
+net start msiserver
+start "\\10.1.3.171\c$\Program Files (x86)\Lansweeper\PackageShare\Installers\Purgar AcroRead service.bat"
+REM Finish..
