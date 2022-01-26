3d35c77c7a83432f75ca46a0ff8fbf7412ee786f
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-26 15:13:44 -0300

    .

---
+++ eliminar REALVNC.bat
@@ -0,0 +1,7 @@
+:: eliminar realvnc server
+:: ubicacion..
+C:\Program Files\RealVNC\VNC Server\unins000.exe /VERYSILENT /NORESTART
+:: eliminar realvnc client
+C:\Program Files\RealVNC\VNC client\unins000.exe /VERYSILENT /NORESTART
+exit
+
