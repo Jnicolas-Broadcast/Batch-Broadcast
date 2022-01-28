1ca6743645722be8f6f4d6fe11966cbfafbbae22
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-28 12:09:00 -0300

    .

--- Wmic-delete-Realvnc6.7.2.42622.bat
+++ Wmic-delete-Realvnc6.7.2.42622.bat
@@ -4,3 +4,5 @@ title Ejecucion wmic
 rem Ejemplar , revisar el uso de comillas simples cuando el paquete lleva espacios
 wmic
 product where "name ='VNC Server 6.7.2'" call uninstall /nointeractive
+quit
+exit
