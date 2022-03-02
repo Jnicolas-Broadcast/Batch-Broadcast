2ed2d96c19b08feead160c5ecebb0d9cc6595526
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-03-02 14:22:09 -0300

    .

--- Deploy office 19 x64.bat
+++ Deploy office 19 x64.bat
@@ -1,5 +1,5 @@
 :: test Deploy de Office2019
-rem xcopy "\\lsserver\packages\Office2019" c:\temp\Office\ /E
+:: xcopy "\\lsserver\packages\Office2019" c:\temp\Office\ /E
 xcopy "{PackageShare}\installers\Office paquete exe setup\" c:\temp\Office\ /E
 :: luego, se ejecuta el "setup" en el equipo local destino, ejemplo:
 :: "c:\temp\Office\setup" /configure "c:\temp\Office\Office Standard 2019.xml"
