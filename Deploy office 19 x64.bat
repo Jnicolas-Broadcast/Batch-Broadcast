09ff4adc9b354224196d1432e4042a41eea0af3a
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-03-04 16:44:27 -0300

    .

--- Deploy office 19 x64.bat
+++ Deploy office 19 x64.bat
@@ -1,13 +1,15 @@
 title Deployment Office 19 (x64)
 :: test Deploy de Office2019 x64
 :: xcopy "\\lsserver\packages\Office2019" c:\temp\Office\ /E
-xcopy "{PackageShare}\installers\Office paquete exe setup\" c:\temp\Office\ /E
+rem xcopy "{PackageShare}\installers\Office paquete exe setup\" c:\temp\Office\ /E
 :: luego, se ejecuta el "setup" en el equipo local destino, ejemplo:
 :: "c:\temp\Office\setup" /configure "c:\temp\Office\Office Standard 2019.xml"
-"C:\temp\Office paquete exe setup\Data\Setup.exe" /configure "C:\temp\Office paquete exe setup\Data\x64Office-dply-config.xml"
-rmdir /S /Q "C:\Temp\Office paquete exe setup"
+rem "C:\temp\Office paquete exe setup\Data\Setup.exe" /configure "C:\temp\Office paquete exe setup\Data\x64Office-dply-config.xml"
+"\\10.1.3.117\utils$\Office paquete exe setup\Data\Setup.exe" /configure "\\10.1.3.117\utils$\Office paquete exe setup\Data\x64Office-dply-config.xml"
+rem rmdir /S /Q "C:\Temp\Office paquete exe setup"
 :: revisar, deberia estar ok..
 :: debido a cómo se quiere instalar Office 2019, TIENE que instalarse como 
 :: el usuario que inició sesión. La cuenta del sistema o las credenciales de escaneo 
 :: no funcionarán, por lo que el usuario debe iniciar sesión en ese momento.
-msg jcabral /server:10.1.6.36 Deploy realizado, revisar su instalación completa en unos minutos.
+msg jcabral /server:10.1.6.36 Deploy en curso, revisar su instalacion completa en unos minutos.
+:: -------------------------------------------------------------------------------------------------
