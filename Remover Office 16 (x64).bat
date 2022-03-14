4e7d54c96033180f40ee33145ec6970ecaee52fb
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-03-14 14:06:06 -0300

    .

--- Remover Office 16 (x64).bat
+++ Remover Office 16 (x64).bat
@@ -1,3 +1,3 @@
 title Remover Office 16 (x64)
-"\\inodoro3\publico\Sistemas\Office 16 Standard\setup.exe" /configure "\\inodoro3\publico\Sistemas\Office 16 Standard\uninstall.xml"
-:: msg jcabral /server:10.1.6.36 Deploy en curso, revisar su instalacion completa en unos minutos.
+"\\inodoro3\publico\Sistemas\Office 16 Standard\setup.exe" /uninstall Standard /config "\\inodoro3\publico\Sistemas\Office 16 Standard\uninstall.xml"
+:: msg jcabral /server:10.1.6.36 Deploy en curso, revisar su desisntalación completa en unos minutos.
