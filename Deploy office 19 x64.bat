5eda64ec0c6ebc82f69cf5eb0c6e775701ffa23c
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-03-16 10:31:24 -0300

    .

--- Deploy office 19 x64.bat
+++ Deploy office 19 x64.bat
@@ -1,4 +1,4 @@
 title Deployment Office 19 (x64)
-msg jcabral /server:10.1.6.36 Deploy Office 2019 x64 en curso, revisar su instalacion completa en unos minutos.
+rem msg jcabral /server:10.1.6.36 Deploy Office 2019 x64 en curso, revisar su instalacion completa en unos minutos.
 "\\inodoro3\publico\Sistemas\Office paquete exe setup\setup" /configure "\\inodoro3\publico\Sistemas\Office paquete exe setup\x64Office-dply-config.xml"
 rem rmdir /S /Q "C:\Temp\Office paquete exe setup"
