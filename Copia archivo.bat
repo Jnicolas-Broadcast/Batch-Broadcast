36dc658ad629f874b1b925e941ad7b580e1d6f16
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-03-25 12:00:57 -0300

    .

---
+++ Copia archivo.bat
@@ -0,0 +1,1 @@
+for /f %a in (HOSTS.txt) do copy /y "C:\Office paquete exe setup" \\%a\c$\
---
+++ Remover Office 19 (x64).bat
@@ -0,0 +1,2 @@
+title Remover Office 19 (x64)
+"\\inodoro3\publico\Sistemas\Office paquete exe setup\setup" /configure "\\inodoro3\publico\Sistemas\Office paquete exe setup\uninstall19.xml"
